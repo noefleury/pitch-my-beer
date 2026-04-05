83b6fb3d693a36a0a80de2d0694ff44a9f8fd038
Noé Fleury <23384755+noefleury@users.noreply.github.com>
2026-04-05 11:57:15 +0200

    style: simple improvement on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,52 @@
 <h1>{{ config('app.name') }}</h1>
 
-<a href="{{ route('materials') }}">{{ trans_choice('material', 2) }}</a>
-<a href="{{ route('beers') }}">{{ trans_choice('beer', 2) }}</a>
-<a href="{{ route('on-taps') }}">{{ __('on taps') }}</a>
-<a href="{{ route('bottlings') }}">{{ __('Bottlings') }}</a>
-<a href="#">todo</a>
+<nav>
+
+    <a href="{{ route('materials') }}" class="link">
+        <span class="btn-link">{{ trans_choice('material', 2) |> ucfirst(...) }}</span>
+    </a>
+    <a href="{{ route('beers') }}" class="link">
+        <span class="btn-link">{{ trans_choice('beer', 2)  |> ucfirst(...) }}</span>
+    </a>
+    <a href="{{ route('on-taps') }}" class="link">
+        <span class="btn-link">{{ __('on taps') |> ucfirst(...) }}</span>
+    </a>
+    <a href="{{ route('bottlings') }}" class="link">
+        <span class="btn-link">{{ __('Bottlings') }}</span>
+    </a>
+
+</nav>
+
+<style>
+
+    nav {
+        display: grid;
+        column-gap: 25px;
+        row-gap: 50px;
+        text-align: center;
+        grid-template-columns: auto auto;
+        @media screen and (max-width: 1500px) {
+            row-gap: 15px;
+            grid-template-columns: auto;
+        }
+    }
+
+    a {
+        text-decoration: none;
+        color: unset;
+    }
+
+    .btn-link {
+        display: block;
+        border: 1px solid gray;
+        border-radius: 5px;
+        padding: 5px;
+        font-size: x-large;
+        cursor: pointer;
+
+        &:hover {
+            background-color: #e3e3e3;
+        }
+    }
+
+</style>
